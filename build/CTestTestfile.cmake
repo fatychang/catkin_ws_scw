fc75c9c255171e979e1f2172dcc764bf8e3c10ba
fatychang <papachang12@gmail.com>
2020-04-20 11:00:12 +0800

    add workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /media/js/data/WFH/Ubuntu/catkin_ws_scw/src
+# Build directory: /media/js/data/WFH/Ubuntu/catkin_ws_scw/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/gtest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /media/js/data/WFH/Ubuntu/catkin_ws_scw/build/gtest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
